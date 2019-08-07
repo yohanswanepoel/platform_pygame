58d5dfd7ca4ee78be9608dd2771b86f530d30d58
Johan Swanepoel <johanswanepoel@Johans-MacBook-Pro.local>
2019-08-07 13:31:53 +1000

    Friction and motion

--- sprites.py
+++ sprites.py
@@ -2,24 +2,43 @@
 import pygame
 from settings import *
 
+vec = pygame.math.Vector2
 
 class Player(pygame.sprite.Sprite):
+
+
 
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((30,40))
         self.image.fill(YELLOW)
         self.rect = self.image.get_rect()
-        self.vx = 0
-        self.vy = 0
+        self.position = vec(WIDTH/2, HEIGHT/2)
+        self.velocity = vec(0, 0)
+        self.acceleration = vec(0, 0)
+        self.rect.center = self.position
+        self.friction = PLAYER_FRICTION
 
     def update(self):
-        self.vx = 0
+        self.acceleration = vec(0, 0)
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
-            self.vx -= 5
+            self.acceleration.x -= PLAYER_ACC
         if keys[pygame.K_RIGHT]:
-            self.vx += 5
+            self.acceleration.x += PLAYER_ACC
 
-        self.rect.x += self.vx
-        self.rect.y += self.vy
+        # Adjust the acceleration by friction
+        # The faster you go the more friction applies
+        self.acceleration += self.velocity * self.friction
+        # Standard equations for motion
+        self.velocity += self.acceleration
+        self.position += self.velocity + PLAYER_ACC * self.acceleration
+        # Do not exit the screen
+        if self.position.x > WIDTH:
+            self.position.x = 0
+        elif self.position.x < 0:
+            self.position.x = WIDTH
+        # Set new position
+        self.rect.center = self.position
+
+
